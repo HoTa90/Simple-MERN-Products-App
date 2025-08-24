6078165bc6398d64c37a3cbe1ed35fcee2d4d57a
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-08-24 20:32:01 +0300

    installed zustand and added create product

---
+++ client/src/hooks/products.js
@@ -0,0 +1,24 @@
+import {create} from 'zustand';
+
+export const useProducts = create((set) => ({
+    products: [],
+    setProducts: (products) => set({products}),
+    createProduct: async(newProduct) => {
+        const {name, price, image} = newProduct;
+
+        if (!name || !price || !image){
+            return {success: false, message: "Please fill all fields." }
+        }
+
+        const response = await fetch ("api/products", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(newProduct)
+        });
+
+        const data = await response.json();
+        set((state) => ({products: [...state.products, data.data]}))
+    }
+}))
